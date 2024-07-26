4e298e1ab2fab2f5c7cd1bb0bd22b8aa1cb3bc41
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-26 23:35:15 +0800

    2024/07/26 set @CrossOrigin

--- src/main/java/com/jack/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/jack/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.jack.ecommerce.dao;
 import com.jack.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCatetory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
